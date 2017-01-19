4016cc076692d96af7de367cc3ebef8b4c8ad857
Raggan <Raggan@users.noreply.github.com>
2017-01-19 15:16:35 +0100

    basic template

---
+++ src/boot.js
@@ -0,0 +1,17 @@
+var Boot = function(game) {
+
+};
+
+Boot.prototype = {
+
+  preload: function(){
+
+  },
+
+  create: function(){
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.game.state.start("Preload");
+  }
+}
